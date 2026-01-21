50ccb7aaf979024ea9732d398772519c1cd9f551
Luke Jackson <luke.hc.jackson@gmail.com>
2026-01-21 17:51:46 +0000

    split calling any unix function into its own function. this is completely function agnostic and works for most commands i tried. you can delete the binary for this program while it is running, change the source code, recompile with gcc, then run the program within itself and the changes work

--- main.c
+++ main.c
@@ -10,11 +10,44 @@
 #define INPUT_BUFFER_SIZE 100
 #define MAX_ARGS 10
 
+int callUnixFunc(int argc, char** argv) {
+
+    pid_t child_pid, wpid;
+    int status = 0;
+    int exitStatus = -1;
+
+    if ((child_pid = fork()) == 0) {
+        exitStatus = execvp(argv[0], argv);
+
+        if (exitStatus == -1) {
+            fprintf(stderr, "COMMAND %s FAILED WITH ARGS: ", argv[0]);
+            for (int i = 0; i < argc; i++) {
+                fprintf(stderr, "%s ", argv[i]);
+            }
+            fprintf(stderr, "\n");
+            exit(1);
+        }
+
+    }
+
+    //need to wait here for the child process to finish
+    while ((wpid = wait(&status)) > 0);
+
+}
+
 int main() {
     
     //display a command prompt interface
     //allow a user to enter text to stdin
     //parse that text and do what they ask for
+
+    #ifdef _WIN32
+        #define PLATFORM_WINDOWS  1
+        #define PLATFORM_UNIX     0
+    #else
+        #define PLATFORM_WINDOWS  0
+        #define PLATFORM_UNIX     1
+    #endif
 
     char input_buffer[INPUT_BUFFER_SIZE];
     char* argv[MAX_ARGS];
@@ -22,6 +55,7 @@ int main() {
     //TODO input_buffer (?) and argv(!!) are not ever memset or reset between commands
         //this does not seem to matter? running ls -lah then ls with no or fewer args gives correct results
     
+    printf("seashell version 2 running");
     while (1) {
         printf("Seashell ~ ");
         //fgets reads from stdin and writes to input_buffer
@@ -52,14 +86,22 @@ int main() {
             //for execvp argv must be null terminated
             argv[argc] = NULL;
 
+            /*
             for (int i = 0; i < argc; i++) {
                 puts(argv[i]);
             }
+            */
             
 
             //now do different things depending on what argv[0] is
             //this would be much easier if we only supported unix but i am writing this on a windows machine
             //so have it do different things based on the OS but with the same seashell command
+
+            #if PLATFORM_UNIX
+                pid_t child_pid, wpid;
+                int status = 0;
+                int exitStatus = -1;
+            #endif
 
             // ls
             if (strcmp(argv[0], "ls") == 0) {
@@ -68,39 +110,59 @@ int main() {
                 //ls [flags]
                 //for a unix system we can just pass that as is, on windows we need to translate between unix flags and windows flags
 
-                #ifdef _WIN32
+                #if PLATFORM_UNIX
+
+                    //todo correctly handle ls argument order: ls [args] [file(s)]
+                    //do i even need to do this? when you type the command into the shell we assume you know what the args are
+                    //and if you give them wrong then we just return the normal exit code / msg ?
+
+                    callUnixFunc(argc, argv);
+
+                #endif
+
+                #if PLATFORM_WINDOWS
+
                     printf("windows ls");
 
                     //TODO
                     //to run anything from the windows command line you need to use MSVC
                     // => spend a day configuring a new C compiler
 
-                #else
+                #endif
+            }
 
-                    pid_t child_pid, wpid;
-                    int status = 0;
+            else if (strcmp(argv[0], "cat") == 0) {
 
-                    if ((child_pid = fork()) == 0) {
-                        int exitStatus = execvp(argv[0], argv);
+                #if PLATFORM_UNIX
 
-                        if (exitStatus == -1) {
-                            fprintf(stderr, "COMMAND %s FAILED WITH ARGS: ", argv[0]);
-                            for (int i = 0; i < argc; i++) {
-                                fprintf(stderr, "%s ", argv[i]);
-                            }
-                            fprintf(stderr, "\n");
-                            exit(1);
-                        }
+                    callUnixFunc(argc, argv);
+                    printf("\n");
 
-                    }
-                    //need to wait here for the child process to finish
-                    while ((wpid = wait(&status)) > 0);
+                #endif
 
+                #if PLATFORM_WINDOWS
+                    printf("windows placeholder");
+                #endif
+
+            }
+
+            else {
+                //just for a laugh run any unix command because this is completely function agnostic at the moment
+                #if PLATFORM_UNIX
+                    callUnixFunc(argc, argv);
                 #endif
             }
 
         }
     }
+
+    //todo:
+    //cannot run mkdir then cd into that folder
+    // up arrow key support
+
+    //amazingly within this program you can delete the binary for it, recompile with gcc, and run it with ./main
+    //can you edit the source code of this program from inside it ???? then recompile ?
+    //YES YOU CAN !
 
     return 0;
 }
